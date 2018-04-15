31532935314fe9ee9daf8c2cc3a5b0f5b7ddee20
afedynitch <afedynitch@gmail.com>
2018-04-15 12:36:09 +0200

    Added utility module for aux. stuff. Info function substitutes print
    Former-commit-id: f04aec9d6aac64509a295f477b97e3e896e42c58

---
+++ impy/util.py
@@ -0,0 +1,61 @@
+"""Utility module for auxiliary methods and classes."""
+
+import inspect
+
+def caller_name(skip=2):
+    """Get a name of a caller in the format module.class.method
+
+    `skip` specifies how many levels of stack to skip while getting caller
+    name. skip=1 means "who calls me", skip=2 "who calls my caller" etc.
+    An empty string is returned if skipped levels exceed stack height.abs
+
+    From https://gist.github.com/techtonik/2151727
+    """
+
+    stack = inspect.stack()
+    start = 0 + skip
+
+    if len(stack) < start + 1:
+        return ''
+
+    parentframe = stack[start][0]
+
+    name = []
+
+    if config["print_module"]:
+        module = inspect.getmodule(parentframe)
+        # `modname` can be None when frame is executed directly in console
+        if module:
+            name.append(module.__name__ + '.')
+
+    # detect classname
+    if 'self' in parentframe.f_locals:
+        # I don't know any way to detect call from the object method
+        # there seems to be no way to detect static method call - it will
+        # be just a function call
+
+        name.append(parentframe.f_locals['self'].__class__.__name__ + '::')
+
+    codename = parentframe.f_code.co_name
+    if codename != '<module>':  # top level usually
+        name.append(codename + '(): ')  # function or a method
+
+    del parentframe
+    return "".join(name)
+
+
+def info(min_dbg_level, *message):
+    """Print to console if `min_debug_level <= config["debug_level"]`
+
+    The fuction determines automatically the name of caller and appends
+    the message to it. Message can be a tuple of strings or objects
+    which can be converted to string using `str()`.
+
+    Args:
+        min_dbg_level (int): Minimum debug level in config for printing
+        message (tuple): Any argument or list of arguments that casts to str
+    """
+
+    if min_dbg_level <= config["debug_level"]:
+        message = [str(m) for m in message]
+        print(caller_name() + " ".join(message))
