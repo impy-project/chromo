28562d1f8af8323a6af667ad30363c030689576e
afedynitch <afedynitch@gmail.com>
2018-04-15 16:53:26 +0200

    Explanations in in the new test
    Former-commit-id: 2f98b157f543cedfae6c94b7ad3a3b5f8fdfb387

--- tests/test_new_interface.py
+++ tests/test_new_interface.py
@@ -1,16 +1,22 @@
+
 import sys
 import os
-sys.path.append(os.path.dirname(__file__) + "/..")
-
 import numpy as np
 
 from impy.constants import *
 from impy.kinematics import EventKinematics
 from impy.models.sibyll import SIBYLLRun
 
+sys.path.append(os.path.dirname(__file__) + "/..")
+
+
+# AF: This is what the user interaction has to yield.
+# It is the typical expected configuration that one
+# wants to run (read pp-mode at energies not exceeding
+# 7 TeV). If you want cosmic ray energies, this should
+# be rather p-N at 10 EeV and lab frame (not yet defined).
 
 eventkinematics = EventKinematics(ecm=7 * TeV, p1pdg=2212, p2pdg=2122)
-
 
 # Something like this can be a template for passing args
 config = dict(
@@ -19,13 +25,28 @@ config = dict(
     event_kinematics=eventkinematics,
 )
 
-# This is a workaround. Something needs to keep track of the
+# This is a workaround. Something needs to keep track of the 
 # fortran imports.
 import sib23c
 
+# Create some wrapper around the fortran library
 generator = SIBYLLRun(sib23c, **config)
-
+# If init remains without args, it should go to the contructor.
 generator.init_generator()
 
+# This 
 for event in generator.event_generator(eventkinematics, 50):
-    print event.px, event.py, event.pz, event.en
+    print 'px', event.px
+    print 'py', event.py 
+    print 'pz', event.pz
+    print 'en', event.en
+
+# AF: Maybe it would be better, or a good, an alternative
+# to make a Particle class and the MCEvent provides an iterator
+# to it's contents as objects of this Particle class. This would
+# be Pythia 8 style.
+
+# for event in ....:
+#     for particle in event:
+#         print particle.p_id, particle.en...
+
