1861feb8cbd1438fcdda9eb93b1bcce4cddf2ed9
Anatoli <afedynitch@gmail.com>
2022-02-08 20:55:45 +0800

    Revert decay afterburner test

--- impy/tests/test_decay_afterburner.py
+++ impy/tests/test_decay_afterburner.py
@@ -2,17 +2,19 @@ import sys
 import os
 import numpy as np
 
-#root_dir = os.path.abspath(os.path.dirname(__file__))
-#sys.path.append(os.path.join(root_dir, '../impy'))
-#sys.path.append(os.path.join(root_dir, '../DPMJET-III-gitlab'))
-sys.path.append('/Users/tkozynets/Software/pythia8306/lib')
-
 from impy.definitions import *
 from impy.constants import *
 from impy.kinematics import EventKinematics
+from impy import impy_config
+
+# root_dir = os.path.abspath(os.path.dirname(__file__))
+# sys.path.append(
+#     os.path.join(os.path.expanduser("~"), "devel", "apps", "pythia8240", "lib")
+# )
 
 # This class will go through the event and decay all particles that should be
 # unstable but did not decay in some other generator
+
 
 class Pythia8DecayAfterburner(object):
     def __init__(self, stable_list):
@@ -20,9 +22,9 @@ class Pythia8DecayAfterburner(object):
         self._init_pythia()
 
     def _init_pythia(self):
-        #pythia_dir = os.path.join(os.path.expanduser('~'), 'devel', 'apps',
-        #                          'pythia8240')
-        #sys.path.append(os.path.join(pythia_dir, 'lib'))
+        pythia_dir = os.path.join(os.path.expanduser('~'), 'devel', 'apps',
+                                 'pythia8240')
+        sys.path.append(os.path.join(pythia_dir, 'lib'))
         import pythia8
 
         self.pythia = pythia8.Pythia()
@@ -37,17 +39,24 @@ class Pythia8DecayAfterburner(object):
         p_ids, status, px, py, pz, en, m = [], [], [], [], [], [], []
 
         for ip in range(event.npart):
-            if event.status[ip] != 1 or abs(
-                    event.p_ids[ip]) in self.stable_list:
+            if event.status[ip] != 1 or abs(event.p_ids[ip]) in self.stable_list:
                 continue
 
             # Set particle to not final and simulate decay
             event.status[ip] = 2
             self.pythia.event.reset()
             # put decaying particle
-            self.pythia.event.append(int(event.p_ids[ip]), 91, 0, 0,
-                                     event.px[ip], event.py[ip], event.pz[ip],
-                                     event.en[ip], event.m[ip])
+            self.pythia.event.append(
+                int(event.p_ids[ip]),
+                91,
+                0,
+                0,
+                event.px[ip],
+                event.py[ip],
+                event.pz[ip],
+                event.en[ip],
+                event.m[ip],
+            )
             self.pythia.particleData.mayDecay(int(event.p_ids[ip]), True)
             # Decay it
             self.pythia.forceHadronLevel()
@@ -64,43 +73,42 @@ class Pythia8DecayAfterburner(object):
                 nappend += 1
 
         append = slice(event.npart, event.npart + nappend)
-        npart = event.npart*1
 
-        event.en = np.append(event.en, en)
-        event.p_ids = np.append(event.p_ids, p_ids)
-        event.status = np.append(event.status, status)
-        event.px = np.append(event.px, px)
-        event.py = np.append(event.py, py)
-        event.pz = np.append(event.pz, pz)
-        event.m = np.append(event.m, m)
+        event.en[append] = en
+        event.p_ids[append] = p_ids
+        event.status[append] = status
+        event.px[append] = px
+        event.py[append] = py
+        event.pz[append] = pz
+        event.m[append] = m
 
         event.npart = nappend
-        event.selection = slice(None, npart)
+        event.selection = slice(None, event.npart)
         event._apply_slicing()
 
 
-# event_kinematics = EventKinematics(ecm=200 * GeV,
-#                                    p1pdg=2212,
-#                                    p2pdg=2212
-#                                    # nuc2_prop=(14,7)
-#                                    )
+event_kinematics = EventKinematics(ecm=200 * GeV,
+                                   p1pdg=2212,
+                                   p2pdg=2212
+                                   # nuc2_prop=(14,7)
+                                   )
 
-# # Watch out this setting!
-# impy_config['pre_slice'] = False
+# Watch out this setting!
+impy_config['pre_slice'] = False
 
-# # The rest is pretty standard
-# generator = make_generator_instance(interaction_model_by_tag['DPMJETIII171'])
-# generator.init_generator(event_kinematics)
+# The rest is pretty standard
+generator = make_generator_instance(interaction_model_by_tag['DPMJETIII191'])
+generator.init_generator(event_kinematics)
 
-# # Here provide the list of particles which you want to retain as stable
-# pythia_afterburner = Pythia8DecayAfterburner(
-#     stable_list=[2212, 11, 12, 14, 15, 16, 22])
+# Here provide the list of particles which you want to retain as stable
+pythia_afterburner = Pythia8DecayAfterburner(
+    stable_list=[2212, 11, 12, 14, 15, 16, 22])
 
-# for event in generator.event_generator(event_kinematics, 200):
-#     # This has to be the first call after an event is generated. The event object
-#     # will be modified and finalized by this call
-#     pythia_afterburner.process_decays(event)
-#     # Here filter only the particles that are remaining as stable
-#     event.filter_final_state()
-#     # Enjoy the result
-#     print 'p_ids', event.p_ids
+for event in generator.event_generator(event_kinematics, 200):
+    # This has to be the first call after an event is generated. The event object
+    # will be modified and finalized by this call
+    pythia_afterburner.process_decays(event)
+    # Here filter only the particles that are remaining as stable
+    event.filter_final_state()
+    # Enjoy the result
+    print('p_ids', event.p_ids)
