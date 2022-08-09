e1ced5ae6da4db8db2f0e0a70dbc5e563fcde38e
Hans Dembinski <hans.dembinski@gmail.com>
2022-08-09 13:28:11 +0200

    fixing test

--- impy/tests/test_all_cms.py
+++ impy/tests/test_all_cms.py
@@ -1,38 +1,16 @@
-from __future__ import print_function
-
-import sys
-import os
 import numpy as np
-from multiprocessing import Pool, freeze_support
+from multiprocessing import Pool, freeze_support, TimeoutError
 import tempfile
-
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(os.path.join(root_dir, "../../apps/pythia8240/lib"))
-
+import pickle
+from pathlib import PurePath as Path
 from impy.constants import GeV
 from impy.kinematics import EventKinematics
 from impy import impy_config
-from impy.util import info
-from impy.models import (
-    Sibyll23d,
-    Sibyll23c,
-    Sibyll23c01,
-    Sibyll23c00,
-    Sibyll23,
-    Sibyll21,
-    DpmjetIII306,
-    DpmjetIII191,
-    EposLHC,
-    Phojet112,
-    Phojet191,
-    UrQMD34,
-    QGSJet01c,
-    QGSJetII03,
-    QGSJetII04,
-)
+import impy.models
+import abc
 
 
-def run_generator(model):
+def run_generator(model, nevents):
     event_kinematics = EventKinematics(
         ecm=7000 * GeV,
         p1pdg=2212,
@@ -45,7 +23,6 @@ def run_generator(model):
     eta_bins = np.linspace(-5, 5, 21)
     eta_widths = eta_bins[1:] - eta_bins[:-1]
     eta_centers = 0.5 * (eta_bins[1:] + eta_bins[:-1])
-    nevents = 5000
     norm = 1.0 / float(nevents) / eta_widths
     hist = np.zeros(len(eta_centers))
     try:
@@ -54,38 +31,32 @@ def run_generator(model):
         for event in generator(nevents):
             event.filter_final_state_charged()
             hist += np.histogram(event.eta, bins=eta_bins)[0]
-        return True, generator.__class__.__name__, log, eta_bins, hist * norm
+        return True, model.__name__, log, eta_bins, hist * norm
     except Exception:
-        return False, generator.__class__.__name__, log, eta_bins, hist * norm
-
-
-models = [
-    Sibyll23d,
-    Sibyll23c,
-    Sibyll23c01,
-    Sibyll23c00,
-    Sibyll23,
-    Sibyll21,
-    DpmjetIII306,
-    DpmjetIII191,
-    EposLHC,
-    Phojet112,
-    Phojet191,
-    UrQMD34,
-    # 'PYTHIA8',
-    QGSJet01c,
-    QGSJetII03,
-    QGSJetII04,
-]
+        return False, model.__name__, log, eta_bins, hist * norm
 
 
 def test_all_cms():
     freeze_support()
-    pool = Pool(processes=32)
-    result = [pool.apply_async(run_generator, (model,)) for model in models]
-    result = [res.get(timeout=1000) for res in result]
 
-    failed = set()
+    # put all models in a list
+    models = []
+    for key in dir(impy.models):
+        obj = getattr(impy.models, key)
+        if isinstance(obj, abc.ABCMeta):
+            models.append(obj)
+
+    result = []
+    with Pool(processes=len(models)) as pool:
+        jobs = [pool.apply_async(run_generator, (model, 100)) for model in models]
+        for job in jobs:
+            try:
+                r = job.get(timeout=1000)
+                result.append(r)
+            except TimeoutError:
+                pass
+
+    failed = set([x.__name__ for x in models])
     passed = set()
     psrap = {}
     logs = {}
@@ -96,22 +67,13 @@ def test_all_cms():
         if r:
             passed.add(gen)
             psrap[gen] = hist
-        else:
-            failed.add(gen)
 
         with open(log) as f:
             logs[gen] = f.read()
 
-    info(0, "Test results for 7 TeV pp collisions in cms frame:\n")
-    info(0, "Passed:", "\n", "\n ".join(passed))
-    info(0, "\nFailed:", "\n", "\n ".join(failed))
+    failed -= passed
 
-    import pickle
+    with open(Path(__file__).stem + ".pkl", "wb") as f:
+        pickle.dump((eta_bins, psrap, logs), f)
 
-    pickle.dump(
-        (eta_bins, psrap, logs),
-        open(os.path.splitext(__file__)[0] + ".pkl", "wb"),
-        protocol=-1,
-    )
-
-    assert passed == set([g.__name__ for g in models])
+    assert not failed
