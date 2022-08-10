2040b640f652757ac91bc992ccea5626a9a02341
Hans Dembinski <hans.dembinski@gmail.com>
2022-08-10 18:38:53 +0200

    ci fix

--- cmake_ext.py
+++ cmake_ext.py
@@ -27,7 +27,7 @@ class CMakeBuild(build_ext):
 
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}/",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-DPython_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",
         ]
 
