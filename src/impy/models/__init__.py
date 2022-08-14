e2766646636d23163f6211e1c5c7b58c1cfbf0eb
Hans Dembinski <hans.dembinski@gmail.com>
2022-08-14 02:18:34 +0200

    missing sib23c04

--- src/impy/models/__init__.py
+++ src/impy/models/__init__.py
@@ -7,6 +7,7 @@ from impy.models.sibyll import (  # noqa
     Sibyll23c01,
     Sibyll23c02,
     Sibyll23c03,
+    Sibyll23c04,
     Sibyll23d,
 )
 from impy.models.dpmjetIII import DpmjetIII191, DpmjetIII193, DpmjetIII306  # noqa
