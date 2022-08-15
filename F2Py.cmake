44532a654f8c29283156611565a76cf80b8527f5
Hans Dembinski <hans.dembinski@gmail.com>
2022-08-15 16:53:58 +0200

    update comments

--- F2Py.cmake
+++ F2Py.cmake
@@ -1,12 +1,16 @@
-# Generate a python module from fortran code with numpy.f2py.
+# Generate a python module from fortran code (optionally) with numpy.f2py.
 #
-# f2py is only used to generate source code, all compilation is done
+# f2py is only used to generate source code. All compilation is done
 # with cmake, to make sure that a consistent single pair of C and Fortran
 # compilers is used. numpy.f2py can also compile the code directly, but it
 # selects the wrong compilers on some machines.
 #
-# f2py generates signature files (.pyf), which a user can edit. If you want
-# to use a pre-generated .pyf file, pass it as part of the INTERFACE_SOURCES.
+# f2py generates an interface file (.pyf), which a user can edit. If you want
+# to use a pre-generated .pyf file, pass it as part of the SOURCES.
+#
+# f2py further generates two files, <target>module.c and 
+# <target>-f2pywrappers.f, which only change if the interface file changes.
+# If you want to use pre-generated files, pass them as part of the SOURCES.
 #
 # This function expects the following variables to be set:
 #
@@ -18,11 +22,15 @@
 #
 # The following arguments are accepted:
 #
-# f2py_add_module(target_name FUNCTIONS [...] INTERFACE_SOURCES [...] SOURCES [...])
+# f2py_add_module(target_name FUNCTIONS [...] INCLUDE_DIRS [...] 
+#                 INTERFACE_SOURCES [...] SOURCES [...])
 #
 # Keyword           : Value
 # ------------------------------------------------------------------------------------
 # FUNCTIONS         : List function names that should be exposed.
+# INCLUDE_DIRS      : Any include directories that the fortran code needs. We cannot
+#                     simply use target_include_directories because f2py also needs
+#                     these files, too, if interface files are to be generated.
 # INTERFACE_SOURCES : List of sources that f2py needs to generate the .pyf file. This
 #                     is a subset of SOURCES. This is optional. If it is not supplied,
 #                     SOURCES is used.
@@ -34,8 +42,8 @@
 # any other target in cmake to change its properties, for example, to set special
 # compiler flags.
 #
-# A log file is generated as a side effect with name ${target_name}.log. The log is
-# placed in the current build directory.
+# A log file is generated as a side effect with name <target>.log. The log is placed
+# in the current build directory.
 
 function (f2py_add_module target_name)
 
@@ -92,7 +100,8 @@ function (f2py_add_module target_name)
 
   if (F2PY_ADD_MODULE_GEN_1 AND F2PY_ADD_MODULE_GEN_2)
     file(APPEND ${F2PY_ADD_MODULE_LOG_FILE} "f2py_add_module: Use existing ${F2PY_ADD_MODULE_GEN_1} ${F2PY_ADD_MODULE_GEN_2}\n")
-    message(STATUS "f2py_add_module: Use existing ${F2PY_ADD_MODULE_GEN_1} ${F2PY_ADD_MODULE_GEN_2}")
+    message(STATUS "f2py_add_module: Use existing ${F2PY_ADD_MODULE_GEN_1}")
+    message(STATUS "f2py_add_module: Use existing ${F2PY_ADD_MODULE_GEN_2}")
   else()
     set(F2PY_ADD_MODULE_GEN_1 ${target_name}module.c)
     set(F2PY_ADD_MODULE_GEN_2 ${target_name}-f2pywrappers.f)
@@ -101,7 +110,8 @@ function (f2py_add_module target_name)
       OUTPUT ${F2PY_ADD_MODULE_GEN_1} ${F2PY_ADD_MODULE_GEN_2}
 
       COMMAND ${PYTHON_EXECUTABLE} -m numpy.f2py
-        ${F2PY_ADD_MODULE_PYF_FILE} ${F2PY_ADD_MODULE_INC}
+        ${F2PY_ADD_MODULE_PYF_FILE}
+        ${F2PY_ADD_MODULE_INC}
         >> ${F2PY_ADD_MODULE_LOG_FILE} 2>&1
 
       DEPENDS ${F2PY_ADD_MODULE_SOURCES} ${F2PY_ADD_MODULE_PYF_FILE}
@@ -110,10 +120,11 @@ function (f2py_add_module target_name)
 
   add_library(${target_name} MODULE
     ${F2PY_INCLUDE_DIR}/fortranobject.c
-    ${F2PY_ADD_MODULE_GEN_1} ${F2PY_ADD_MODULE_GEN_2}
+    ${F2PY_ADD_MODULE_GEN_1}
+    ${F2PY_ADD_MODULE_GEN_2}
     ${F2PY_ADD_MODULE_SOURCES}
   )
-  if (PYTHON_LIBRARIES) # may neither be available nor required
+  if (PYTHON_LIBRARIES) # may not be available (e.g. on manylinux)
     target_link_libraries(${target_name} PRIVATE ${PYTHON_LIBRARIES})
   endif()
   if (F2PY_ADD_MODULE_INCLUDE_DIRS)
