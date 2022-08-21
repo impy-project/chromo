8fbb6e17ec1f4f999d959c537860494cbc8e493e
Hans Dembinski <hans.dembinski@gmail.com>
2022-08-21 16:43:26 +0200

    add benchmark for event

---
+++ bench/test_new_event.py
@@ -0,0 +1,41 @@
+from impy import models as im
+from impy.constants import TeV
+from impy.kinematics import EventKinematics
+import pytest
+
+N = 1000
+ekin = EventKinematics(ecm=10 * TeV, p1pdg=2212, p2pdg=2212)
+models = {name: getattr(im, name)(ekin) for name in ("Sibyll21", "Pythia6")}
+
+
+@pytest.mark.parametrize("model", models)
+def test_event(model, benchmark):
+    model = models[model]
+
+    def run(model):
+        for event in model(N):
+            pass
+
+    benchmark(run, model)
+
+
+@pytest.mark.parametrize("model", models)
+def test_event_final(model, benchmark):
+    model = models[model]
+
+    def run(model):
+        for event in model(N):
+            event.final_state()
+
+    benchmark(run, model)
+
+
+@pytest.mark.parametrize("model", models)
+def test_event_final_charged(model, benchmark):
+    model = models[model]
+
+    def run(model):
+        for event in model(N):
+            event.final_state_charged()
+
+    benchmark(run, model)
