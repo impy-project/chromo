94a8ecdff93fcce66bd1dd589b3f06e53c4fe5e4
Anton Prosekin <ayprosekin@gmail.com>
2022-10-18 12:15:34 +0800

    Fix linking problem

--- F2Py.cmake
+++ F2Py.cmake
@@ -170,6 +170,7 @@ function (f2py_add_module target_name)
     PRIVATE ${F2PY_ADD_MODULE_COMPILE_DEFS})
   endif()  
   target_compile_options(${target_name} PRIVATE -cpp)
+  target_link_libraries(${target_name} PUBLIC "-static")
   set_property(TARGET ${target_name} PROPERTY SUFFIX ${PYTHON_MODULE_EXTENSION})
   # must be a string, so that empty string works correcty
   set_property(TARGET ${target_name} PROPERTY PREFIX "${PYTHON_MODULE_PREFIX}")
