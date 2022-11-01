d714e5a421bec5e4d6391bf4514a64a6ad323765
Anton Prosekin <ayprosekin@gmail.com>
2022-11-01 19:02:40 +0800

    Changes to make CI work

--- setup.py
+++ setup.py
@@ -3,17 +3,19 @@ from setuptools import setup
 import sys
 import subprocess as subp
 import platform
+import os
 
 cwd = Path(__file__).parent
 
 sys.path.append(str(cwd))
 from cmake_ext import CMakeExtension, CMakeBuild  # noqa: E402
 
-if (cwd / ".git").exists():
-    # make sure that submodules are up-to-date,
-    # it is a common error to forget this when
-    # switching between development branches
-    subp.check_call(["git", "submodule", "update"])
+if not os.environ.get("CI", False):
+    if (cwd / ".git").exists():
+        # make sure that submodules are up-to-date,
+        # it is a common error to forget this when
+        # switching between development branches
+        subp.check_call(["git", "submodule", "update"])
 
 models = [
     "eposlhc",
