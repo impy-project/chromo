fix and improve test, guard against buffer overflow

src/impy/writer.py
@@ -1,9 +1,8 @@
 import numpy as np
-from impy.constants import quarks_and_diquarks_and_gluons, GeV, millibarn
+from impy.constants import quarks_and_diquarks_and_gluons, millibarn, GeV
 import dataclasses
 from pathlib import Path
 
-BUFFER_SIZE = 100000
 INT_TYPE = np.int32
 FLOAT_TYPE = np.float32
 
@@ -37,25 +36,28 @@ def _raise_import_error(name, task):
 # so we don't write them. Long-lived particles are final state, and there is no
 # interesting information in the vertices of very short-lived particles.
 class Root:
-    def __init__(self, file, config, model, write_vertices=False):
+    def __init__(self, file, config, model, write_vertices=False, buffer_size=100000):
         try:
             import uproot
         except ModuleNotFoundError:
             _raise_import_error("uproot", "write ROOT files")
+
+        assert GeV == 1
+        assert millibarn == 1
 
         # FIXME projectile_momentum and target_momentum are 0 if -S option is used,
         # to be fixed in follow-up PR that fixes EventKinematics
         header = {
             "seed": model.seed,
             "projectile_id": config.projectile_id,
-            "projectile_momentum": config.projectile_momentum / GeV,
+            "projectile_momentum": config.projectile_momentum,
             "target_id": config.target_id,
-            "target_momentum": config.target_momentum / GeV,
+            "target_momentum": config.target_momentum,
             "model": model.label,
         }
         header.update(
             {
-                f"sigma_{k}": v / millibarn
+                f"sigma_{k}": v
                 for (k, v) in dataclasses.asdict(model.cross_section()).items()
                 if not np.isnan(v)
             }
@@ -63,36 +65,36 @@ class Root:
         header.update(
             {
                 "energy_unit": "GeV",
-                "length_unit": "mm",
                 "sigma_unit": "mb",
             }
         )
 
-        self._header = "\n".join(f"{k}: {v}" for (k, v) in header.items())
         self._file = uproot.recreate(file)
 
         self._event_buffers = {
-            "impact": np.empty(BUFFER_SIZE, FLOAT_TYPE),
+            "impact": np.empty(buffer_size, FLOAT_TYPE),
         }
         self._particle_buffers = {
-            "px": np.empty(BUFFER_SIZE, FLOAT_TYPE),
-            "py": np.empty(BUFFER_SIZE, FLOAT_TYPE),
-            "pz": np.empty(BUFFER_SIZE, FLOAT_TYPE),
-            "m": np.empty(BUFFER_SIZE, FLOAT_TYPE),
-            "pdgid": np.empty(BUFFER_SIZE, INT_TYPE),
-            "status": np.empty(BUFFER_SIZE, INT_TYPE),
-            "parent": np.empty(BUFFER_SIZE, INT_TYPE),
+            "px": np.empty(buffer_size, FLOAT_TYPE),
+            "py": np.empty(buffer_size, FLOAT_TYPE),
+            "pz": np.empty(buffer_size, FLOAT_TYPE),
+            "m": np.empty(buffer_size, FLOAT_TYPE),
+            "pdgid": np.empty(buffer_size, INT_TYPE),
+            "status": np.empty(buffer_size, INT_TYPE),
+            "parent": np.empty(buffer_size, INT_TYPE),
         }
         if write_vertices:
+            header["length_unit"] = "mm"
             self._particle_buffers.update(
                 {
-                    "vx": np.empty(BUFFER_SIZE, FLOAT_TYPE),
-                    "vy": np.empty(BUFFER_SIZE, FLOAT_TYPE),
-                    "vz": np.empty(BUFFER_SIZE, FLOAT_TYPE),
-                    "vt": np.empty(BUFFER_SIZE, FLOAT_TYPE),
+                    "vx": np.empty(buffer_size, FLOAT_TYPE),
+                    "vy": np.empty(buffer_size, FLOAT_TYPE),
+                    "vz": np.empty(buffer_size, FLOAT_TYPE),
+                    "vt": np.empty(buffer_size, FLOAT_TYPE),
                 }
             )
 
+        self._header = "\n".join(f"{k}: {v}" for (k, v) in header.items())
         self._tree = None
         self._lengths = []
         self._iparticle = 0
@@ -152,11 +154,19 @@ class Root:
         mask[:2] = False
         event = event[mask]
 
+        event_size = len(event)
+        buffer_size = len(self._particle_buffers["px"])
+        if self._iparticle + event_size > buffer_size:
+            self._write_buffers()
+            if event_size > buffer_size:
+                # this should never happen
+                raise RuntimeError("event is larger than buffer")
+
         ievent = len(self._lengths)
         self._event_buffers["impact"][ievent] = getattr(event, "impact_parameter", 0.0)
 
         a = self._iparticle
-        self._iparticle += len(event)
+        self._iparticle += event_size
         b = self._iparticle
         self._lengths.append(b - a)
         for key, val in self._particle_buffers.items():
@@ -166,11 +176,6 @@ class Root:
                 val[a:b] = event.pid
             else:
                 val[a:b] = getattr(event, key)
-                if key in ("px", "py", "pz", "m"):
-                    val[a:b] *= 1e3
-
-        if b > BUFFER_SIZE // 2:
-            self._write_buffers()
 
 
 class Svg: