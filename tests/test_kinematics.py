667a273d72e37f4792db6d5abf19813ca1f289b5
Hans Dembinski <HDembinski@users.noreply.github.com>
2023-01-14 03:35:21 +0100

    Fix kinematics (#112)

--- tests/test_kinematics.py
+++ tests/test_kinematics.py
@@ -5,34 +5,15 @@ from impy.kinematics import (
     Momentum,
     EventFrame,
     CompositeTarget,
+    GeV,
+    MeV,
 )
-from impy.constants import GeV, MeV
+from impy.constants import nucleon_mass
+from impy.util import AZ2pdg, energy2momentum
 from particle import literals as lp
 from pytest import approx
-
-
-def test_fixed_target():
-    ft = FixedTarget(10, "proton", "proton")
-    assert ft.elab == 10 * GeV
-    assert ft.frame == EventFrame.FIXED_TARGET
-
-    ft = FixedTarget(10.0 * GeV, "proton", "proton")
-    assert ft.elab == 10 * GeV
-    assert ft.frame == EventFrame.FIXED_TARGET
-
-    ft = FixedTarget(TotalEnergy(2 * GeV), "proton", "proton")
-    assert ft.elab == 2 * GeV
-    assert ft.frame == EventFrame.FIXED_TARGET
-
-    ft = FixedTarget(KinEnergy(2 * GeV), "proton", "proton")
-    et = 2 + (lp.proton.mass * MeV)
-    assert ft.elab == approx(et)
-    assert ft.frame == EventFrame.FIXED_TARGET
-
-    ft = FixedTarget(Momentum(2 * GeV), "proton", "proton")
-    et = (2**2 + (lp.proton.mass * MeV) ** 2) ** 0.5
-    assert ft.elab == approx(et)
-    assert ft.frame == EventFrame.FIXED_TARGET
+import pytest
+import numpy as np
 
 
 def test_CompositeTarget_repr():
@@ -46,3 +27,57 @@ def test_CompositeTarget_repr():
 
     t = CompositeTarget([("N", 3), ("O", 1)], label="air")
     assert repr(t) == "CompositeTarget([('N14', 0.75), ('O16', 0.25)], label='air')"
+
+
+def test_fixed_target():
+    x = 2 * GeV
+
+    ft = FixedTarget(TotalEnergy(x), "proton", "proton")
+    assert ft.plab < x
+    assert ft.elab == x
+    assert ft.frame == EventFrame.FIXED_TARGET
+    # default is to interpret x as total energy
+    assert ft == FixedTarget(x, "proton", "proton")
+
+    ft = FixedTarget(KinEnergy(x), "proton", "proton")
+    et = x + (lp.proton.mass * MeV)
+    assert ft.elab == approx(et, rel=1e-3)
+    assert ft.frame == EventFrame.FIXED_TARGET
+
+    ft = FixedTarget(Momentum(x), "proton", "proton")
+    et = (x**2 + (lp.proton.mass * MeV) ** 2) ** 0.5
+    assert ft.plab == x
+    assert ft.elab > x
+    assert ft.elab == approx(et, rel=1e-3)
+    assert ft.frame == EventFrame.FIXED_TARGET
+
+    ft = FixedTarget(x, "proton", "He")
+    assert ft.p1 == lp.proton.pdgid
+    assert ft.p2 == AZ2pdg(4, 2)
+    # check that ecm is in nucleon-nucleon collision system
+    p1 = np.array([energy2momentum(x, lp.proton.mass * MeV), x])
+    p2 = np.array([0, nucleon_mass])
+    ps = p1 + p2
+    ecm = (ps[1] ** 2 - ps[0] ** 2) ** 0.5
+    assert ft.ecm == approx(ecm, rel=1e-3)
+
+    x = 32 * GeV
+    ft = FixedTarget(x, "O", "He")
+    assert ft.p1 == AZ2pdg(16, 8)
+    assert ft.p2 == AZ2pdg(4, 2)
+    # check that ecm is in nucleon-nucleon collision system
+    p1 = np.array([energy2momentum(x, nucleon_mass), x])
+    p2 = np.array([0, nucleon_mass])
+    ps = p1 + p2
+    ecm = (ps[1] ** 2 - ps[0] ** 2) ** 0.5
+    assert ft.ecm == approx(ecm, rel=1e-3)
+
+
+def test_fixed_target_bad_input():
+    with pytest.raises(ValueError):
+        FixedTarget(0.1 * GeV, "p", "p")
+
+    t = CompositeTarget([("N", 3), ("O", 1)])
+
+    with pytest.raises(ValueError):
+        FixedTarget(100 * GeV, t, "p")
--- tests/test_util.py
+++ tests/test_util.py
@@ -2,6 +2,7 @@ from impy import util
 import numpy as np
 from numpy.testing import assert_equal
 from particle import literals as lp
+from pytest import approx
 
 
 def test_select_parents():
@@ -58,3 +59,42 @@ def test_Nuclei():
     assert carbon in d
     assert lead not in d
     assert repr(d) == "Nuclei(a_min=1, a_max=20, z_min=0, z_max=1000)"
+
+
+def test_ecm_elab_conversion():
+    elab = 2.1
+    m1 = 1.1
+    m2 = 0.5
+    ecm = util.elab2ecm(elab, m1, m2)
+    elab2 = util.ecm2elab(ecm, m1, m2)
+    assert elab2 == approx(elab)
+
+
+def test_momentum_energy_conversion():
+    p = 2.1
+    m = 1.1
+    en = util.momentum2energy(p, m)
+    p2 = util.energy2momentum(en, m)
+    assert p == approx(p2)
+
+
+def test_is_real_nucleus():
+    proton = 2212
+    neutron = 2112
+
+    assert not util.is_real_nucleus(proton)
+    assert not util.is_real_nucleus(neutron)
+
+    proton2 = util.AZ2pdg(1, 1)
+    neutron2 = util.AZ2pdg(1, 0)
+
+    assert not util.is_real_nucleus(proton2)
+    assert not util.is_real_nucleus(neutron2)
+
+    deuterium = util.AZ2pdg(2, 1)
+
+    assert util.is_real_nucleus(deuterium)
+
+    mix = util.CompositeTarget([("p", 0.5), ("He", 0.5)])
+
+    assert util.is_real_nucleus(mix)
