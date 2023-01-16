38d18a50b472f7fa93d737f0677ebd5cd497c2eb
Hans Dembinski <hans.dembinski@gmail.com>
2023-01-16 10:35:58 +0100

    get version from metadata

--- src/impy/__init__.py
+++ src/impy/__init__.py
@@ -1,8 +1,10 @@
-from impy.version import __version__
 from impy import models
 from impy import kinematics
 from impy import constants
 import os
+from importlib.metadata import version
+
+__version__ = version("impy")
 
 debug_level = int(os.environ.get("DEBUG", "0"))
 
