b1368aa79ea55bd2d4163fdf91abf71a70dc1796
Anton Prosekin <ayprosekin@gmail.com>
2023-02-16 11:50:18 +0800

    fix argument number problem

--- src/chromo/common.py
+++ src/chromo/common.py
@@ -715,7 +715,7 @@ class MCRun(ABC):
         """
         self.set_stable(pdgid, False)
 
-    def cross_section(self, kin=None, precision=None):
+    def cross_section(self, kin=None, *, precision=None):
         """Cross sections according to current setup.
 
         Parameters
@@ -738,7 +738,10 @@ class MCRun(ABC):
                         setattr(cross_section, field, val)
                 return cross_section
             else:
-                return self._cross_section(kin, precision)
+                if precision:
+                    return self._cross_section(kin, precision)
+                else:
+                    return self._cross_section(kin)
 
     @abstractmethod
     def _cross_section(self, kin, *args):
