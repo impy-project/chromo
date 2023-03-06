c37d4aaf81b0cdd30434ed0eda49a6eb16ca864d
Anton Prosekin <ayprosekin@gmail.com>
2023-03-06 15:19:48 +0800

    Simplify condition

--- src/chromo/models/__init__.py
+++ src/chromo/models/__init__.py
@@ -6,7 +6,6 @@ from chromo.models.sibyll import (
     Sibyll23d,
 )
 
-
 from chromo.models.dpmjetIII import DpmjetIII306
 from chromo.models.epos import EposLHC
 from chromo.models.qgsjet import QGSJet01d, QGSJetII03, QGSJetII04
