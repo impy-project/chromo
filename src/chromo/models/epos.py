0629d14c13f95663d8ac43cabd55fdc1e98640ea
Anton Prosekin <ayprosekin@gmail.com>
2023-07-26 13:14:17 +0800

    remove unused line

--- src/chromo/models/epos.py
+++ src/chromo/models/epos.py
@@ -13,12 +13,6 @@ from chromo.constants import standard_projectiles
 class EPOSEvent(MCEvent):
     """Wrapper class around EPOS particle stack."""
 
-    def __init__(self, generator):
-        super().__init__(generator)
-        # EPOS sets mothers of beam particles to (-1, -1).
-        # We change it to (0, 0)
-        self.mothers[self.status == 4] = -1
-
     def _charge_init(self, npart):
         return self._lib.charge_vect(self._lib.hepevt.idhep[:npart])
 
