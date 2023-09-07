f82aebb66757956a350823c78477461af2466fdd
gaudu <gaudu@uni-wuppertal.de>
2023-09-07 17:02:11 +0200

    Fixed few first error messages from Pythia 8.3.08 -> 8.3.10.

--- src/cpp/_pythia8.cpp
+++ src/cpp/_pythia8.cpp
@@ -2,7 +2,9 @@
 #include <Pythia8/HIUserHooks.h>
 #include <Pythia8/Info.h>
 #include <Pythia8/ParticleData.h>
-#include <Pythia8/Pythia.h>
+#include <Pythia8/Info.h>
+#include <Pythia8/HIInfo.h>
+// #include <Pythia8/UserHooks.h>
 #include <array>
 #include <cassert>
 #include <limits>
@@ -248,7 +250,7 @@ PYBIND11_MODULE(_pythia8, m)
         .def(py::init<string, bool>(), py::call_guard<py::scoped_ostream_redirect, py::scoped_estream_redirect>())
         .def("init", &Pythia::init, py::call_guard<py::scoped_ostream_redirect, py::scoped_estream_redirect>())
         .def("next", py::overload_cast<>(&Pythia::next))
-        .def("readString", &Pythia::readString, "setting"_a, "warn"_a = true)
+        .def("readString", &Pythia::readString, "setting"_a, "warn"_a = true, "subrun"_a = SUBRUNDEFAULT)
         .def("forceHadronLevel", &Pythia::forceHadronLevel, "find_junctions"_a = true)
         .def_readwrite("particleData", &Pythia::particleData)
         .def_readwrite("settings", &Pythia::settings)
