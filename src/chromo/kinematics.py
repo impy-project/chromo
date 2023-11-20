1c58169d7cfee46768656b03c6b901e4952d670d
afedynitch <afedynitch@gmail.com>
2023-11-21 00:06:59 +0800

    Add pcm to event kinematics for fw and hash

--- src/chromo/kinematics.py
+++ src/chromo/kinematics.py
@@ -76,6 +76,7 @@ class EventKinematicsBase:
     p1: Union[PDGID, Tuple[int, int]]
     p2: Union[PDGID, Tuple[int, int], CompositeTarget]
     ecm: float  # for ions this is nucleon-nucleon collision system
+    pcm: float
     plab: float
     elab: float
     ekin: float
@@ -119,6 +120,19 @@ class EventKinematicsBase:
             return a == b
 
         return all(eq(a, b) for (a, b) in zip(at, bt))
+
+    def __hash__(self):
+        li = []
+        for k in dataclasses.astuple(self):
+            if (
+                isinstance(k, tuple)
+                and isinstance(k[0], np.ndarray)
+                and isinstance(k[1], np.ndarray)
+            ):
+                li.append((tuple(k[0]), tuple(k[1])))
+            else:
+                li.append(k)
+        return hash(tuple(li))
 
     def copy(self):
         return EventKinematicsBase(
@@ -234,13 +248,26 @@ class EventKinematics(EventKinematicsBase):
             for b, m in zip(beams, (m1, m2)):
                 b[3] = np.sqrt(m**2 + b[2] ** 2)
 
-        _gamma_cm = (elab + m2) / ecm
-        _betagamma_cm = plab / ecm
+        gamma_cm = (elab + m2) / ecm
+        betagamma_cm = plab / ecm
+        pcm = np.sqrt((ecm**2 - (m1 + m2) ** 2) * (ecm**2 - (m1 - m2) ** 2)) / (
+            2 * ecm
+        )
         self.m1 = m1
         self.m2 = m2
 
         super().__init__(
-            frame, part1, part2, ecm, plab, elab, ekin, beams, _gamma_cm, _betagamma_cm
+            frame,
+            part1,
+            part2,
+            ecm,
+            pcm,
+            plab,
+            elab,
+            ekin,
+            beams,
+            gamma_cm,
+            betagamma_cm,
         )
 
         self._beam_data = None
