ef78d2d48ef19c5c9fa914b008d247c114423ca6
Anton Prosekin <ayprosekin@gmail.com>
2024-05-01 20:57:57 +0800

    refactor code

--- src/fortran/rangen.c
+++ src/fortran/rangen.c
@@ -2,7 +2,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-// Define the structure for a 128-bit integer
 typedef struct {
     // The structure is defined the same as in 
     // "numpy/numpy/random/src/pcg64/pcg64.h"
@@ -34,62 +33,28 @@ void npynxt_(double* value, int64_t* ptr)
     *value = bg->next_double(bg->state);
 }
 
-
+// called from Fortran
 void npynxt_get_state_(uint64_t* state_arr, int64_t* ptr)
 {
     bitgen_t* bg = (bitgen_t*)(*ptr);
     pcg64_state *state = (pcg64_state *)bg->state;
-    state_arr[0] = state->pcg_state->state.high;
-    state_arr[1] = state->pcg_state->state.low;
-    state_arr[2] = state->pcg_state->inc.high;
-    state_arr[3] = state->pcg_state->inc.low;
-
-    // printf("State High: %lu\n", state->pcg_state->state.high);
-    // printf("State Low: %lu\n", state->pcg_state->state.low);
-    // printf("Inc High: %lu\n", state->pcg_state->inc.high);
-    // printf("Inc Low: %lu\n", state->pcg_state->inc.low);
+    pcg_state_setseq_128* pcg_state128 = state->pcg_state;
+    state_arr[0] = pcg_state128->state.high;
+    state_arr[1] = pcg_state128->state.low;
+    state_arr[2] = pcg_state128->inc.high;
+    state_arr[3] = pcg_state128->inc.low;
 }
 
 void npynxt_set_state_(uint64_t* state_arr, int64_t* ptr)
 {
     bitgen_t* bg = (bitgen_t*)(*ptr);
     pcg64_state *state = (pcg64_state *)bg->state;
+    pcg_state_setseq_128* pcg_state128 = state->pcg_state;
 
-    // printf("Before setting:");
-    // printf("State High: %lu\n", state->pcg_state->state.high);
-    // printf("State Low: %lu\n", state->pcg_state->state.low);
-    // printf("Inc High: %lu\n", state->pcg_state->inc.high);
-    // printf("Inc Low: %lu\n", state->pcg_state->inc.low);
-
-    state->pcg_state->state.high = state_arr[0];
-    state->pcg_state->state.low = state_arr[1];
-    state->pcg_state->inc.high = state_arr[2];
-    state->pcg_state->inc.low = state_arr[3];
-
-    // printf("After setting:");
-    // printf("State High: %lu\n", state->pcg_state->state.high);
-    // printf("State Low: %lu\n", state->pcg_state->state.low);
-    // printf("Inc High: %lu\n", state->pcg_state->inc.high);
-    // printf("Inc Low: %lu\n", state->pcg_state->inc.low);
-
-
-
-    // pcg_state_setseq_128 pstate = state->pcg_state
-    // pstate.state
-    // pcg_state* state = (pcg_state*)bg->state;
-    // printf("%d", *(int64_t*)bg->state->state);
-
-    // Extract the high and low parts of state and inc
-    // state_values[0] = state->pcg_state->state.high;
-    // state_values[1] = state->pcg_state->state.low;
-    // state_values[2] = state->pcg_state->inc.high;
-    // state_values[3] = state->pcg_state->inc.low;
-
-    // printf("State High: %lu\n", state->pcg_state->state.high);
-    // printf("State Low: %lu\n", state->pcg_state->state.low);
-    // printf("Inc High: %lu\n", state->pcg_state->inc.high);
-    // printf("Inc Low: %lu\n", state->pcg_state->inc.low);
-
+    pcg_state128->state.high = state_arr[0];
+    pcg_state128->state.low = state_arr[1];
+    pcg_state128->inc.high = state_arr[2];
+    pcg_state128->inc.low = state_arr[3];
 }
 
 // called from Fortran
