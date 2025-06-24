e2c51b78622b1151b40fcb3904679bbc1ab13c19
Anatoli Fedynitch <afedynitch@gmail.com>
2025-06-24 12:21:12 +0800

    Fix pre-commit

--- scripts/list_cc_files.py
+++ scripts/list_cc_files.py
@@ -2,4 +2,4 @@ import sys
 from pathlib import Path
 
 src_dir = Path(sys.argv[1])
-print("\n".join(str(p) for p in src_dir.rglob("*.cc")))
+print("\n".join(str(p) for p in src_dir.rglob("*.cc")))  # noqa: T201
