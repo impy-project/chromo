ca885455c58df0ca58c889b007bf22d3a47fddba
Anatoli <afedynitch@gmail.com>
2025-06-26 11:10:42 +0800

    Fix

--- src/chromo/models/pythia8.py
+++ src/chromo/models/pythia8.py
@@ -93,7 +93,7 @@ class Pythia8(MCRun):
     _restartable = True
     _data_url = (
         "https://github.com/impy-project/chromo"
-        "/releases/download/zipped_data_v1.0/Pythia8_v004.zip"
+        "/releases/download/zipped_data_v1.0/Pythia8_v005.zip"
     )
 
     def __init__(self, evt_kin, *, seed=None, config=None, banner=True):
